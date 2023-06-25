5f96a0ab2206567d25ec9c5eeb66d8cb89ac0846
nicolasprudencio <nicolas-prudencio@hotmail.com>
2023-06-24 21:07:18 -0300

    ft_printf v2.0

--- ft_printf.c
+++ ft_printf.c
@@ -6,73 +6,75 @@
 /*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 12:16:54 by nprudenc          #+#    #+#             */
-/*   Updated: 2023/06/22 20:16:52 by nprudenc         ###   ########.fr       */
+/*   Updated: 2023/06/24 21:06:56 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include "ft_printf.h"
-#include <stdio.h>
-
-
 
 int ft_printf(const char *str, ...)
 {	
 	va_list list;
 	int		i;
-	char	c;
-	int		j;
-	char	*s;
+	int		counter;
 
 	i = 0;
-	j = 0;
+	counter = 0;
 	va_start(list, str);
 	while (str[i])
 	{	
-		if (str[i] == '%' && str[i + 1] == 'c')
-		{	
-			c = va_arg(list, int);
-			ft_putchar(c);
-			i++;
-		}
-		else if (str[i] == '%' && str[i + 1] == 's')
-		{	
-			s = va_arg(list, char *);
-			ft_putstr(s);
-			i++;
-		}
-		else if (str[i] == '%' && str[i + 1] == 'i')
-		{	
-			j = va_arg(list, int);
-			ft_putnbr(j);
-			i++;
-		} 
-		else
+		if (str[i] == '%')
 		{
-			ft_putchar(str[i]);
+			i++;
+			if (str[i] == 'c')
+				counter += ft_putchar(va_arg(list, int));
+			if (str[i] == '%')
+				counter += ft_putchar('%');
+			if (str[i] == 's')
+				counter += ft_putstr(va_arg(list, char *));
+			if (str[i] == 'i' || str[i] == 'd')
+				counter += ft_putnbr(va_arg(list, int));
+			if (str[i] == 'u')
+				counter += ft_print_long(va_arg(list, int));
+			if (str[i] == 'x')
+				counter += ft_print_hex(va_arg(list, unsigned int), 'x');
+			if (str[i] == 'X')
+				counter += ft_print_hex(va_arg(list, unsigned int), 'X');
 		}
+		else
+			counter += ft_putchar(str[i]);
 		i++;
-		// if (str[i] == '\n')
-		// {	
-		// 	ft_putchar('\n');
-		// 	i++;
-		// }
 	}
 	va_end(list);
-	return (0);
+	return (counter);
 }
 
-int	main(void)
-{	
-	char	c;
-	// char	*str = "hellow";
-	int		i;
+ /*
+	organizar sua estrutura para a função final
+	retornar a quantidade exata de caracteres que
+	foram impresso.
 
-	i = -1234;
-	c = 'A';
-	i = 7.5;
-	// ft_printf("meu char %i", i);
-	printf("double: %d", i);
-	i ^= 3;
-	return (0);
-}
+	além disso você deve reduzir essas condicionais para ocuparem
+	somente uma linha.
+	-> separar cada caso para uma função exclusiva
+
+ */
+
+// int	main(void)
+// {	
+// 	char	c;
+// 	// char	*str = NULL;
+// 	int		i;
+// 	int		teste;
+// 	// int		j;
+
+// 	i = '\0';
+// 	c = 'A';
+// 	// j = 7.5;
+// 	teste = ft_printf(" %u %u %u %u %u %u %u", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, -42);
+// 	ft_printf("\nMine: %i\n\n", teste);
+// 	// teste = printf("holy moly: %d",	ULONG_MAX);
+// 	// printf("\nprintf: %i", teste);
+// 	return (0);
+// }
