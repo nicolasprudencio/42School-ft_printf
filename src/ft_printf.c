3e244c7fe5d18f04e08ad64016218fc7062acd0e
nicolasprudencio <nicolas-prudencio@hotmail.com>
2023-06-26 12:27:39 -0300

    ft_printf: concluded

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,16 +6,40 @@
 /*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 12:16:54 by nprudenc          #+#    #+#             */
-/*   Updated: 2023/06/24 21:06:56 by nprudenc         ###   ########.fr       */
+/*   Updated: 2023/06/26 12:26:22 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
 #include "ft_printf.h"
+#include <stdarg.h>
 
-int ft_printf(const char *str, ...)
+int	ft_select_char(char c, va_list list)
+{		
+	int	counter;
+
+	counter = 0;
+	if (c == 'c')
+		counter += ft_putchar(va_arg(list, int));
+	else if (c == '%')
+		counter += ft_putchar('%');
+	else if (c == 's')
+		counter += ft_putstr(va_arg(list, char *));
+	else if (c == 'i' || c == 'd')
+		counter += ft_putnbr(va_arg(list, int));
+	else if (c == 'u')
+		counter += ft_print_long(va_arg(list, int));
+	else if (c == 'x')
+		counter += ft_print_hex(va_arg(list, unsigned int), 'x');
+	else if (c == 'X')
+		counter += ft_print_hex(va_arg(list, unsigned int), 'X');
+	else if (c == 'p')
+		counter += ft_print_ptr(va_arg(list, unsigned long));
+	return (counter);
+}
+
+int	ft_printf(const char *str, ...)
 {	
-	va_list list;
+	va_list	list;
 	int		i;
 	int		counter;
 
@@ -25,23 +49,7 @@ int ft_printf(const char *str, ...)
 	while (str[i])
 	{	
 		if (str[i] == '%')
-		{
-			i++;
-			if (str[i] == 'c')
-				counter += ft_putchar(va_arg(list, int));
-			if (str[i] == '%')
-				counter += ft_putchar('%');
-			if (str[i] == 's')
-				counter += ft_putstr(va_arg(list, char *));
-			if (str[i] == 'i' || str[i] == 'd')
-				counter += ft_putnbr(va_arg(list, int));
-			if (str[i] == 'u')
-				counter += ft_print_long(va_arg(list, int));
-			if (str[i] == 'x')
-				counter += ft_print_hex(va_arg(list, unsigned int), 'x');
-			if (str[i] == 'X')
-				counter += ft_print_hex(va_arg(list, unsigned int), 'X');
-		}
+			counter += ft_select_char(str[++i], list);
 		else
 			counter += ft_putchar(str[i]);
 		i++;
@@ -49,32 +57,3 @@ int ft_printf(const char *str, ...)
 	va_end(list);
 	return (counter);
 }
-
- /*
-	organizar sua estrutura para a função final
-	retornar a quantidade exata de caracteres que
-	foram impresso.
-
-	além disso você deve reduzir essas condicionais para ocuparem
-	somente uma linha.
-	-> separar cada caso para uma função exclusiva
-
- */
-
-// int	main(void)
-// {	
-// 	char	c;
-// 	// char	*str = NULL;
-// 	int		i;
-// 	int		teste;
-// 	// int		j;
-
-// 	i = '\0';
-// 	c = 'A';
-// 	// j = 7.5;
-// 	teste = ft_printf(" %u %u %u %u %u %u %u", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, -42);
-// 	ft_printf("\nMine: %i\n\n", teste);
-// 	// teste = printf("holy moly: %d",	ULONG_MAX);
-// 	// printf("\nprintf: %i", teste);
-// 	return (0);
-// }
