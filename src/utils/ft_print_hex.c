b6dc734dc514bcd6424761fb1d7afc029bad9b97
nicolasprudencio <nicolas-prudencio@hotmail.com>
2023-06-27 18:20:25 -0300

    removing unused variables

--- src/utils/ft_print_hex.c
+++ src/utils/ft_print_hex.c
@@ -6,7 +6,7 @@
 /*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 13:19:48 by nprudenc          #+#    #+#             */
-/*   Updated: 2023/06/26 12:03:57 by nprudenc         ###   ########.fr       */
+/*   Updated: 2023/06/27 18:19:56 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,10 @@
 int	ft_print_hex(unsigned int nb, char x)
 {
 	int	letter_counter;
-	int	index;
 
-	index = 0;
 	letter_counter = 0;
 	if (nb / 16 > 0)
 		letter_counter += ft_print_hex(nb / 16, x);
-	nb = nb % 16;
 	if (x == 'X')
 		letter_counter += ft_putchar("0123456789ABCDEF"[nb % 16]);
 	else
